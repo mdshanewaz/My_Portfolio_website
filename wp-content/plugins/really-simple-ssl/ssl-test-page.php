ff4621d2a337dffeeabf7033d236c3b2220f8f40
mdshanewaz <mdshanewazdevp@gmail.com>
2021-01-28 12:51:43 +0600

    My_Portfolio

---
+++ wp-content/plugins/really-simple-ssl/ssl-test-page.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+</head>
+<body>
+<h1>#SSL TEST PAGE#</h1>
+<p>This page is used purely to test for SSL availability.</p>
+#SERVER-HTTPS-ON# (on)<br>#SERVERPORT443#<br>#LOADBALANCER#<br><br>#SUCCESSFULLY DETECTED SSL#
+</body>
+</html>
---
+++ wp-content/plugins/really-simple-ssl/system-status.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://mdshanewaz.github.io/My_Portfolio_Website/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://mdshanewaz.github.io/My_Portfolio_Website/";
+		</script>
+
+		<p>You are being redirected to <a href="https://mdshanewaz.github.io/My_Portfolio_Website/">https://mdshanewaz.github.io/My_Portfolio_Website/</a></p>
+	</body>
+</html>
